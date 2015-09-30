ea7330a083b45f41292a0ec217bc98cb5bafd83b
Cecilia Testart <ctestart@Cecilias-MacBook-Pro.local>
2015-09-30 00:53:25 -0400

    Latest version of paper and images

--- src/finiteDifference.py
+++ src/finiteDifference.py
@@ -26,6 +26,13 @@ def sumQuadBowl(X):
 	'''Quadratic bowl function including sum of elements'''
 	return (np.sum(X.T*X))
 
+def sumQuadBowlGrad(X):
+	'''The analytical gradient of the function'''
+	Grad=np.zeros_like(X)
+	Grad[0]=2*X[0]
+	Grad[1]=2*X[1]
+	return Grad
+
 def nonConvexFunction(X):
 	'''A non-convex function (3x^4-8x^3+5x^2+y^2) to test gradient descent'''
 	x=X[0]
@@ -42,13 +49,17 @@ def nonConvexFunctionGrad(X):
 	return Grad
 
 
-def numGradDescent(F, X_0, step, threshold, spacing, analytical=False, iterations=0):
+def numGradDescent(F, X_0, step, threshold, spacing, analytical=False, QuadBowl=True , iterations=0):
 	'''looks for the minimum of function F using the gradient descent method starting '''
 	''' in X_0, using the step for next X if the threshold in no attained, evaluating '''
 	'''the gradient numerically '''
 	F_X0=F(X_0)
 	print ('F('+str(X_0)+')='+str(F_X0))
-	if analytical:
+	if (analytical and QuadBowl):
+		gradF_X0=sumQuadBowlGrad(X_0)
+		print ('Analytical Grad: ',end=' ')
+		print (gradF_X0)
+	elif (analytical and not QuadBowl):
 		gradF_X0=nonConvexFunctionGrad(X_0)
 		print ('Analytical Grad: ',end=' ')
 		print (gradF_X0)
@@ -66,29 +77,29 @@ def numGradDescent(F, X_0, step, threshold, spacing, analytical=False, iteration
 		print ('Last Grad: ', end='')
 		print (gradF_X0)
 		return (F_X1)
-	return numGradDescent(F,X_1,step, threshold, spacing, analytical, (iterations+1))
+	return numGradDescent(F,X_1,step, threshold, spacing, analytical,QuadBowl, (iterations+1))
 
 if __name__ == '__main__':
 	X=np.array([4,8])
-	step= 0.5
+	step= 0.25
 	thresh=0.001
 	sp=0.5
 	# print (sumQuadBowl(X))
 	# print (Finite_Diff(sumQuadBowl,X,0.5))
 	# print (fmin_bfgs(sumQuadBowl, X))
-	# print ('\nGradient Descent')
-	# print ('Initial Guess'+ str(X)+'\t\t Step: '+str(step))
-	# print ('Threshold: '+str(thresh)+'\t\t Spacing: '+str(sp))
-	# numGradDescent(sumQuadBowl,X,step,thresh, sp)
-	print ('\nNon-Convex Function gradient descent')
-	Y=np.array([0,2])
-	print (Finite_Diff(nonConvexFunction,Y,sp))
-	print ('Analytical Grad: '+str(nonConvexFunctionGrad(Y)))
-	F_Y=nonConvexFunction(Y)
-	print ('X= '+str(Y)+'\t\t F(X)= '+str(F_Y))
-	print ('Initial Guess'+ str(Y)+'\t\t Step: '+str(step))
+	print ('\nQuad Bowl Gradient Descent')
+	print ('Initial Guess'+ str(X)+'\t\t Step: '+str(step))
 	print ('Threshold: '+str(thresh)+'\t\t Spacing: '+str(sp))
-	numGradDescent(nonConvexFunction,Y,step,thresh, sp, True)
+	numGradDescent(sumQuadBowl,X,step,thresh, sp, True, True)
+	# print ('\nNon-Convex Function gradient descent')
+	# Y=np.array([0,2])
+	# print (Finite_Diff(nonConvexFunction,Y,sp))
+	# print ('Analytical Grad: '+str(nonConvexFunctionGrad(Y)))
+	# F_Y=nonConvexFunction(Y)
+	# print ('X= '+str(Y)+'\t\t F(X)= '+str(F_Y))
+	# print ('Initial Guess'+ str(Y)+'\t\t Step: '+str(step))
+	# print ('Threshold: '+str(thresh)+'\t\t Spacing: '+str(sp))
+	# numGradDescent(nonConvexFunction,Y,step,thresh, sp, True, False)
 	
 
 
